f15ddeb77eb715a3e5fed32c28e7e377fa6e28c3
Amir <sehabiamir1@gmail.com>
2023-03-16 19:36:13 +0100

    changed orders table container

--- Components/OrdersPage/components/ordersTable/OrdersTableContainer.tsx
+++ Components/OrdersPage/components/ordersTable/OrdersTableContainer.tsx
@@ -1,4 +1,4 @@
-import { Container, Table, useMantineColorScheme } from "@mantine/core";
+import { Container, ScrollArea, Table, useMantineColorScheme } from "@mantine/core";
 import { useAtomValue } from "jotai";
 import type { NextComponentType, NextPageContext } from "next";
 import { IconContext } from "react-icons";
@@ -55,23 +55,64 @@ const OrdersTableContainer: NextComponentType<NextPageContext, {}, Props> = (
 
                 className={style.Animated_Background_Gradient}
 
+                mih={"100vh"}
                 py={"xl"}
+                mt={"5rem"}
+                fluid
+                maw={1500}
             >
 
 
-                <Table highlightOnHover withColumnBorders withBorder
-
+                <ScrollArea
+                    sx={{
+                        border: `2px solid ${colorScheme === "dark"
+                            ? CardContainerColors.borderColorDark
+                            : CardContainerColors.borderColorLight}`,
+                        borderRadius: 15,
+                        WebkitBackdropFilter: "blur(2px)",
+                        boxShadow: "0 4px 30px rgba(0, 0, 0, 0.5)",
+                    }}
                 >
-                    <thead>
-                        <tr>
-                            <th>Email</th>
-                            <th>Delivery Type</th>
-                            <th>Payment Method</th>
-                            {/* <th></th> */}
-                        </tr>
-                    </thead>
-                    <tbody>{rows}</tbody>
-                </Table>
+                    <Table highlightOnHover withColumnBorders>
+                        <thead>
+                            <tr>
+                                <th
+                                    style={{
+                                        color: colorScheme === "dark"
+                                            ? CardContainerColors.iconsLineColorDark
+                                            : CardContainerColors.iconsLineColorLight,
+                                    }}
+                                >
+                                    Email
+                                </th>
+
+                                <th
+                                    style={{
+                                        color: colorScheme === "dark"
+                                            ? CardContainerColors.iconsLineColorDark
+                                            : CardContainerColors.iconsLineColorLight,
+                                    }}
+                                >
+                                    Delivery Type
+                                </th>
+
+                                <th
+                                    style={{
+                                        color: colorScheme === "dark"
+                                            ? CardContainerColors.iconsLineColorDark
+                                            : CardContainerColors.iconsLineColorLight,
+                                    }}
+                                >
+                                    Payment Method
+                                </th>
+
+                                {/* <th></th> */}
+                            </tr>
+                        </thead>
+                        <tbody>{rows}</tbody>
+                    </Table>
+                </ScrollArea>
+
             </Container>
 
         </IconContext.Provider>
