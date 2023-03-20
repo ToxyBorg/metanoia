ab6ad22e374fa1869c403fe5cab3f7e273539984
Amir <sehabiamir1@gmail.com>
2023-03-20 18:51:49 +0100

    Update BackgroundParticles.tsx

--- Components/MainPage/BackgroundParticles.tsx
+++ Components/MainPage/BackgroundParticles.tsx
@@ -35,7 +35,7 @@ const BackgroundParticles: NextComponentType<NextPageContext, {}, Props> = (
                 //         value: "#0d47a1",
                 //     },
                 // },
-                fpsLimit: 30,
+                fpsLimit: 60,
                 interactivity: {
                     events: {
                         onClick: {
